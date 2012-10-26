29b91c2fdc877a82d2172260818bc37a88016697
richharriman <mainerefugee@gmail.com>
2012-10-26 10:23:10 -0400

    cleaned up the code to make it easier to read

--- hw1-3.js
+++ hw1-3.js
@@ -1,34 +1,30 @@
 var express = require('express')
 var mongo = require('mongodb');
-var ObjectID = require('mongodb').ObjectID;
 var app = express();
 
-var url, db, dbconn;
-url = "mongodb://localhost:27017/m101?safe=true";
-
-db = mongo.connect(url, function(err, conn) {
-
+var db, dbconn;
+db = mongo.connect("mongodb://localhost:27017/m101?safe=true", function(err, conn) {
     conn.on('error', function(err) {
-        return console.log('%s: Mongo connect error %s',Date(Date.now() ), err);});
+        return console.log('%s: Mongo connect error %s',Date(Date.now() ), err);}
+    );
     dbconn = conn;
 });
 
-function find_number(request, response){
+function find_number(req, resp){
 
-    var n = parseInt(request.param("n"));
+    var n = parseInt(req.param("n"));
 
     dbconn.collection("funnynumbers").find({}).skip(n).limit(1).sort({"value":1}).toArray(function(err,doc){
         if (err){
             console.err("Error trying to read collection:"+err);
         }
         else {
-            response.send(""+doc[0].value+"\r\n");
+            resp.send(doc[0].value.toString()+"\r\n");
         }
 
     });
 }
 
 app.get('/hw1/:n', find_number);
-
 app.listen(8080);
 console.log('Express listening on port 8080');
